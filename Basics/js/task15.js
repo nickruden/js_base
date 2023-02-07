851d89051b71487adbd2be6cdd41feb899a6e308
nickruden <1nikitos.ru2018@gmail.com>
2023-02-07 20:24:48 +0300

    Сложение строк в JavaScript(task15)

---
+++ Basics/js/task15.js
@@ -0,0 +1,13 @@
+//Ex. 1
+let str = "!!!"
+alert("Строка со значением - " + str);
+
+//Ex. 2
+let j = "java";
+let s = "script";
+alert(j + s);
+
+//Ex. 3
+let h = "hello";
+let w = "world";
+alert(h + w);
