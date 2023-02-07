64a4d237bc23ea72c7ae82113acf25f0e29597a9
nickruden <1nikitos.ru2018@gmail.com>
2023-02-07 21:02:42 +0300

    Работа с консолью в JavaScript(Task_19)

---
+++ Basics/js/task19.js
@@ -0,0 +1,20 @@
+//Ex. 1
+let a = 1;
+console.log("Вывод переменной в консоль - " + a);
+
+//Ex. 2
+let b = 49;
+let c = "arr";
+let d = 85;
+console.log("Вывод трёх переменных в консоль - " + b, c, d)
+
+//Ex. 3
+let num = 12;
+let str = "know";
+let t = true;
+let un = undefined;
+console.log("Вывод перменных с разными типами данных в консоль - " + num, str, t, un);
+
+//Ex. 4
+let n = 67;
+console.log(n + h);
