53a76abc2b6693c40ba64cddf34de32950050c5c
nickruden <1nikitos.ru2018@gmail.com>
2023-02-08 16:41:18 +0300

    Строки с цифрами в JavaScript(Task_29)

---
+++ Basics/js/task29.js
@@ -0,0 +1,18 @@
+//Ex. 1
+let str1 = "12345";
+alert("Упражнение №1. Сумма цифр встроке '" + str1 + "' равна " + (Number(str1[0]) + Number(str1[1]) + Number(str1[2]) + Number(str1[3]) + Number(str1[4])));
+
+//Ex. 2
+let num1 = 12345;
+let numstr1 = String(num1);
+alert("Упражнение №2. Сумма цифр в числе " + num1 + " равна " + (Number(numstr1[0]) + + Number(numstr1[1]) + + Number(numstr1[2]) + + Number(numstr1[3]) + + Number(numstr1[4])));
+
+//Ex. 3
+let num2 = 12345;
+let numstr2 = String(num2);
+alert("Упражнение №3. Произведение цифр в числе " + num2 + " равно " + (numstr2[0] * numstr2[1] * numstr2[2] * numstr2[3] * numstr2[4]));
+
+//Ex. 4
+let num3 = 12345;
+let numstr3 = String(num3);
+alert("Упражнение №4. Число " + num3 + " с цифрами в обратном порядке равно " + numstr3[4] + numstr3[3] + numstr3[2] + numstr3[1] + numstr3[0]);
