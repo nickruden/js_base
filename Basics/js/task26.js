b26692104413555e964ff3d8622a345872fdf44a
nickruden <1nikitos.ru2018@gmail.com>
2023-02-08 16:13:23 +0300

    Преобразование к логическому типу в JavaScript(Task_26)

---
+++ Basics/js/task26.js
@@ -0,0 +1,104 @@
+/* Ex. 1
+let test = Boolean(3);
+alert(test); */
+alert("Упражнение №1. true");
+
+/* Ex. 2 
+let test = Boolean(0);
+alert(test); */
+alert("Упражнение №2. false");
+
+/* Ex. 3 
+let test = Boolean(-1);
+alert(test); */
+alert("Упражнение №3. true");
+
+/* Ex. 4 
+let test = Boolean(-0);
+alert(test); */
+alert("Упражнение №4. false");
+
+/* Ex. 5 
+let test = Boolean(+0);
+alert(test); */
+alert("Упражнение №5. fasle");
+
+/* Ex. 6 
+let test = Boolean('abc');
+alert(test); */
+alert("Упражнение №6. true");
+
+/* Ex. 7 
+let test = Boolean('');
+alert(test); */
+alert("Упражнение №7. false");
+
+/* Ex. 8 
+let test = Boolean('0');
+alert(test); */
+alert("Упражнение №8. true");
+
+/* Ex. 9 
+let test = Boolean(true);
+alert(test); */
+alert("Упражнение №9. true");
+
+/* Ex. 10 
+let test = Boolean(false);
+alert(test); */
+alert("Упражнение №10. false");
+
+/* Ex. 11 
+let test = Boolean('true');
+alert(test); */
+alert("Упражнение №11. true");
+
+/* Ex. 12  
+let test = Boolean('false');
+alert(test); */
+alert("Упражнение №12. ture");
+
+/* Ex. 13  
+let test = Boolean(null);
+alert(test); */
+alert("Упражнение №13. false");
+
+/* Ex. 14
+let test = Boolean('null');
+alert(test); */
+alert("Упражнение №14. true");
+
+/* Ex. 15
+let test = Boolean(undefined);
+alert(test); */
+alert("Упражнение №15. false");
+
+/* Ex. 16
+let test = Boolean('undefined');
+alert(test); */
+alert("Упражнение №16. true");
+
+/* Ex. 17
+let test = Boolean(NaN);
+alert(test); */
+alert("Упражнение №17. false");
+
+/* Ex. 18
+let test = Boolean('NaN');
+alert(test); */
+alert("Упражнение №18. true");
+
+/* Ex. 19 
+let test = Boolean(3 * 'abc');
+alert(test); */
+alert("Упражнение №19. false");
+
+/* Ex. 20
+let test = Boolean(Infinity);
+alert(test); */
+alert("Упражнение №20. true");
+
+/*  Ex. 21
+let test = Boolean(1 / 0);
+alert(test); */
+alert("Упражнение №21. true");
