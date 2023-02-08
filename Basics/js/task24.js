0dab8fa76b9463df70ac5218b1f884e70a1ce3ea
nickruden <1nikitos.ru2018@gmail.com>
2023-02-08 16:11:37 +0300

    Преобразование к строке в JavaScript(Task_24)

---
+++ Basics/js/task24.js
@@ -0,0 +1,16 @@
+//Ex. 1
+let x = 24;
+let y = 19;
+let sumstr = String(x) + String(y);
+alert("Упражнение №1. Вывод сложения двух чисел как строки: " + sumstr);
+
+//Ex. 2
+let num = 152948
+alert("Упражнение №2. Колличество цифр в этом числе равно: " + String(num).length);
+
+//Ex. 3
+let num1 = 253;
+let num2 = 16;
+let count1 = String(num1).length;
+let count2 = String(num2).length;
+alert("Упражнение №3. Сумма колличества цифр в числе " + num1 + " и числе " + num2 + " равна " + (count1 + count2));
