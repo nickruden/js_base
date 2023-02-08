02b935c3c0103f86010e9b5aa1ac851d826edb9b
nickruden <1nikitos.ru2018@gmail.com>
2023-02-08 16:00:44 +0300

    Выделение чисел в JavaScript(Task_23)

---
+++ Basics/js/task23.js
@@ -0,0 +1,14 @@
+//Ex. 1
+let a = '5px';
+let b = '6px';
+alert('Упражнение №1. Вывод суммы целого числа пикселей: ' + (parseInt(a) + parseInt(b)));
+
+//Ex. 2
+let a2 = '5.5px';
+let b2 = '6.25px';
+alert("Упражнение №2. Вывод суммы дробного числа пикселей: " + (parseFloat(a2) + parseFloat(b2)));
+
+//Ex. 3
+let a3 = '5.5px';
+let b3 = '6.25px';
+alert("Упражнение №2. Вывод суммы дробного числа пикселей: " + (parseFloat(a2) + parseFloat(b2)) + " px");
