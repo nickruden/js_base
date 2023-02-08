3a5ec94da33b24a04b20b415d6b8d823ae120b7e
nickruden <1nikitos.ru2018@gmail.com>
2023-02-08 16:12:40 +0300

    Преобразование логического типа в JavaScript(Task_25)

---
+++ Basics/js/task25.js
@@ -0,0 +1,27 @@
+/* Ex. 1
+alert(true + 3); */
+alert("Упражнение №1. Выведет 4");
+
+/* Ex. 2 
+alert(true + true); */
+alert("Упражнение №2. Выведет 2");
+
+/* Ex. 3 
+alert(true - true); */
+alert("Упражнение №3. Выведет 0");
+
+/* Ex. 4 
+alert(true + false); */
+alert("Упражнение №4. Выведет 1");
+
+/* Ex. 5 
+alert('1' + true); */
+alert("Упражнение №5. Выведет '11'");
+
+/* Ex. 6 
+alert( String(true) + 1 ); */
+alert("Упражнение №6. Выведет 'true1' ");
+
+/* Ex. 7 
+alert( String(true) + Number(true) ); */
+alert("Упражнение №7. Выведет 'true1'");
