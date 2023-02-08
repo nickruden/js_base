93ba7185a1873b150b0b8106a0ce70ed6f4f6871
nickruden <1nikitos.ru2018@gmail.com>
2023-02-08 15:51:23 +0300

    Преобразование к числу в JavaScript(Task_22)

---
+++ Basics/js/task22.js
@@ -0,0 +1,26 @@
+// Ex. 1
+let a1 = "10";
+let b1 = "20";
+let res = (Number(a1) + Number(b1));
+alert("Упражнение №1. Выведет " + res);
+
+/* Ex. 2 
+alert( Number('2') + Number('3') ); */
+alert("Упражнение №2. Выведет 5 ");
+
+/* Ex. 3 
+alert( 2 + Number('3') ); */
+alert("Упражнение №3. Выведет 5");
+
+/* Ex. 4 
+alert( '2' + Number('3') ); */
+alert("Упражнение №4. Выведет '23'");
+
+/* Ex. 5 
+let a2 = '2';
+let b2 = '3';
+alert(a + b); */
+
+let a2 = +'2';
+let b2 = +'3';
+alert("Упражнение №5. " + (a2 + b2)); 
