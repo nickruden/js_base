c932b323c1cedaf94a39e1681c5553b52381b178
nickruden <1nikitos.ru2018@gmail.com>
2023-02-11 16:58:09 +0300

    Операции инкремента и декремента в JavaScript(Task_31)

---
+++ Basics/js/task31.js
@@ -0,0 +1,65 @@
+// Ex. 1
+let num = 10;
+// num = num + 1;
+num++;
+// num = num + 1;
+++num;
+// num = num - 1;
+num--;
+alert(num);
+
+/* Ex. 2 
+let num = 3;
+alert(++num); */
+alert("Упражнение №2. На экран выведет 4");
+
+/* Ex. 3 
+let num = 3;
+alert(num++); */
+alert("Упражнение №3. На экран выведет 3");
+
+/* Ex. 4 
+let num = 3;
+alert(--num); */
+alert("Упражнение №4. На экран выведет 2");
+
+/* Ex. 5 
+let num = 3;
+alert(num--); */
+alert("Упражнение №5. На экран выведет 3");
+
+/* Ex. 6 
+let num1 = 3;
+let num2 = ++num1;
+alert(num1);
+alert(num2); */
+alert("Упражнение №6. На экран выведет сначала 4 и потом 4");
+
+/* Ex. 7 
+let num1 = 3;
+let num2 = num1++;
+alert(num1);
+alert(num2); */
+alert("Упражнение №7. На экран выведет сначала 4, а потом 3");
+
+/* Ex. 8 
+let num1 = 3;
+let num2 = --num1;
+alert(num1);
+alert(num2); */
+alert("Упражнение №8. На экран выведет сначала 2 и потом 2");
+
+/* Ex. 9 
+let num1 = 3;
+let num2 = num1--;
+alert(num1);
+alert(num2); */
+alert("Упражнение №9. На экран выведет сначала 2, а потом 3");
+
+/* Ex. 10
+let num1 = 3;
+num1++;
+let num2 = num1--;
+alert(num1++);
+alert(--num2); */
+alert("Упражнение №10. На экран выведет сначала 3, а потом 3");
