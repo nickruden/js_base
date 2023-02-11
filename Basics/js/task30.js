d5bb22a72470794cf7e8d1b4f532957a6fc04a38
nickruden <1nikitos.ru2018@gmail.com>
2023-02-11 16:45:09 +0300

    Сокращенные операции в JavaScript(Task_30)

---
+++ Basics/js/task30.js
@@ -0,0 +1,27 @@
+/* Ex. 1 
+let num = 1;
+num = num + 1;
+num = num + 1;
+
+alert(num); */
+alert("Упражнение №1. На экран выведется 3")
+
+/* Ex. 2 
+let num = 1;
+num = num + 2;
+num = num + 3;
+
+alert(num); */
+alert("Упражнение №2. На экран выведется 6")
+
+// Ex. 3
+let num = 47;
+// num = num + 7;
+num += 7;
+// num = num - 18;
+num -= 18;
+// num = num * 10;
+num *= 10;
+// num = num / 15;
+num /= 15;
+alert("Упражнение №3. " + num);
