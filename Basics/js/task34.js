69f6d3b45f777eb045e1007cf86551509453a3f7
nickruden <1nikitos.ru2018@gmail.com>
2023-02-11 17:33:30 +0300

    Вывод текста в документ в JavaScript(Task_34)

--- Basics/js/task34.js
+++ Basics/js/task34.js
@@ -1,12 +1,12 @@
 //Ex. 1
-document.write("Упражнение №1. Какой-то текст.<hr>")
+document.write("Упражнение №1. Какой-то текст. <br> <br> <br>")
 
 //Ex. 2
-document.write("<p> Упражнение №2. Какой-то текст.</p> <hr>")
+document.write("Упражнение №2. <p>Какой-то текст.</p>")
 
 //Ex. 3
 let str = 'text';
-document.write("<p>" + str + "</p> <hr>");
+document.write("Упражнение №3. <p>" + str + "</p>");
 
 //Ex. 4
-document.write(1  + "<br>" + 2 + "<br>" + 3 + "<br>" + 4 + "<br>" + 5 + "<br>" + "<hr>");
+document.write("Упражнение №4. " + "<br>" + 1  + "<br>" + 2 + "<br>" + 3 + "<br>" + 4 + "<br>" + 5 + "<br>");
