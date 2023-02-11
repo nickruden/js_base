979c326b3e951c6b96d89c8421e1245dcd1c48ca
nickruden <1nikitos.ru2018@gmail.com>
2023-02-11 17:52:12 +0300

    Поиск ошибок в коде(Task_35)

---
+++ Basics/js/task35.js
@@ -0,0 +1,64 @@
+// Ex. 1
+let num1 = 1;
+let num2 = 2;
+console.log("Упражнение №1. " + 'сумма: ' + (num1 + num2));
+
+// Ex. 2
+let a1 = 1;
+let b1 = 2;
+console.log( "Упражнение №2. " + (a1 + b1));
+
+// Ex. 3
+let num3 = '123';
+let sum = Number(num3[0]) + Number(num3[1]) + Number(num3[2]);
+console.log("Упражнение №3. " + sum);
+
+// Ex. 4
+let num4 = String(123);
+console.log("Упражнение №4. " + num4[0]);
+
+// Ex. 5
+let a2 = 0;
+console.log("Упражнение №5. " + (++a2));
+
+// Ex. 6
+let num5 = String(123);
+console.log("Упражнение №6. " + num5.length);
+
+// Ex. 7
+let a3 = 24 * 60 * 60;
+console.log("Упражнение №7. " + a3);
+
+// Ex. 8
+let num6 = 123;
+let str1 = String(num6);
+console.log("Упражнение №8. " + str1.length);
+
+// Ex. 9
+let num7 = 123;
+let str2 = String(num7);
+console.log("Упражнение №9. " + str2[str2.length-1])
+
+// Ex. 10
+let num8 = 123;
+let str3 = String(num8);
+console.log("Упражнение №10. " + str3.length);
+
+// Ex. 11
+let num9 = 123;
+let str4 = String(num9);
+console.log("Упражнение №11. " + str4[str4.length-1]);
+
+// Ex. 12
+let a = '123';
+let b = '456';
+let s = Number(a) + Number(b);
+
+console.log("Упражнение №12. " + s);
+
+// Ex. 13
+let aaa = 1;
+let bbb = 2;
+let ccc = 3;
+
+console.log("Упражнение №13. " + (aaa + bbb + ccc));
