ed77be86eeb5978a6fbf99d03cd1353034468ce5
nickruden <1nikitos.ru2018@gmail.com>
2023-02-15 13:16:40 +0300

    Ограничения на ключи объектов в JavaScript(Task_52)

---
+++ Objects/js/task52.js
@@ -0,0 +1,24 @@
+// Ex. 1
+let obj1 = {
+	'1a': 1,
+	'b2': 2,
+	'с-с': 3,
+	'd 4': 4,
+	'e5': 5
+};
+console.log(`Упражнение №1. Ключи '1a', 'c-c', 'd 4' - необходимо поставить в кавычки`)
+
+// Ex. 2
+let obj2 = {
+	'1a': 1,
+	'b2': 2,
+	'c-c': 3,
+	'd 4': 4,
+	'e5': 5
+};
+
+console.log(obj2['1a']);
+console.log(obj2.b2);
+console.log(obj2['c-c']);
+console.log(obj2['d 4']);
+console.log(obj2.e5);
