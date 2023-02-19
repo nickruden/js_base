b07d0492704946b6acbd936bec2815e1232ce540
nickruden <1nikitos.ru2018@gmail.com>
2023-02-19 15:51:31 +0300

    Ошибка обращения к элементу по ключу в JavaScript(Task_59)

---
+++ Objects/js/task59.js
@@ -0,0 +1,13 @@
+/* Ex. 1
+let obj1 = {x: 1, y: 2, z: 3};
+console.log(obj1[x]); */
+let obj1 = {x: 1, y: 2, z: 3};
+console.log(obj1['x']);
+
+/* Ex. 2
+let obj2 = {x: 1, y: 2, z: 3};
+let key = 'x';
+console.log(obj2['key']); */
+let obj2 = {x: 1, y: 2, z: 3};
+let key = 'x';
+console.log(obj2[key]);
