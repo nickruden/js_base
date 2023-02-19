981ec7809dff4aac7fe290533625bdbf0a87ea85
nickruden <1nikitos.ru2018@gmail.com>
2023-02-19 15:59:41 +0300

    Ошибка обращения к свойству объекта в JavaScript(Task_60)

---
+++ Objects/js/task60.js
@@ -0,0 +1,20 @@
+/* Ex. 1
+let obj = {x: 1, y: 2, z: 3};
+
+let prop = 'x';
+console.log(obj.prop); */
+let obj1 = {x: 1, y: 2, z: 3};
+
+let prop1 = 'x';
+console.log(obj1[prop1]);
+
+
+/* Ex. 2 
+let obj = {x: 1, y: 2, z: 3};
+
+let prop = 'x';
+console.log(obj['prop']); */
+let obj2 = {x: 1, y: 2, z: 3};
+
+let prop2 = 'x';
+console.log(obj2[prop2]);
