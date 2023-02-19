c25641348f5fde3ef36f51a31b809de9964af1d3
nickruden <1nikitos.ru2018@gmail.com>
2023-02-19 16:27:24 +0300

    Типизация объектов в JavaScript(Task_64)

---
+++ Objects/js/task64.js
@@ -0,0 +1,23 @@
+/* Ex. 1
+console.log( typeof {x: 1, y: 2, z: 3} ); */
+console.log( typeof {x: 1, y: 2, z: 3} ); 
+alert("Упражнение №1. В консоль выведется object")
+
+/* Ex. 2
+console.log( typeof {} ); */
+console.log( typeof {} );
+alert("Упражнение №2. В консоль выведется object")
+
+/* Ex. 3
+let obj1 = {x: 1, y: 2, z: 3};
+console.log( typeof obj1 ); */
+let obj1 = {x: 1, y: 2, z: 3};
+console.log( typeof obj1 );
+alert("Упражнение №3. В консоль выведется object")
+
+/* Ex. 4
+let obj2 = {x: 1, y: 2, z: 3};
+console.log( typeof obj2['x'] ); */
+let obj2 = {x: 1, y: 2, z: 3};
+console.log( typeof obj2['x'] );
+alert("Упражнение №4. В консоль выведется number")
