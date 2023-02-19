269e3c55611640ef64ba7eb2feebef4c5a30598a
nickruden <1nikitos.ru2018@gmail.com>
2023-02-19 16:39:27 +0300

    Отличаем массивы от объектов в JavaScript(Task_66)

---
+++ Objects/js/task66.js
@@ -0,0 +1,9 @@
+/* Ex. 1 
+console.log( Array.isArray([1, 2, 3]) ); */ 
+console.log( Array.isArray([1, 2, 3]) );
+alert("Упражнение №1. В консоль выведется true, так как в функцию передаётся массив");
+
+/* Ex. 2 
+console.log( Array.isArray({x: 1, y: 2, z: 3}) ); */ 
+console.log( Array.isArray({x: 1, y: 2, z: 3}) ); 
+alert("Упражнение №2. В консоль выведется false, так как в функцию передаётся объект");
