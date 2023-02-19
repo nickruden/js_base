9c838b14e568c16b81d560ef9688cc4606de44e6
nickruden <1nikitos.ru2018@gmail.com>
2023-02-19 17:08:31 +0300

    Объекты и примитивы в JavaScript(Task_67)

---
+++ Objects/js/task67.js
@@ -0,0 +1,45 @@
+/* Ex. 1
+let test1 = {x: 1, y: 2, z: 3};
+console.log(test1); */
+let test1 = {x: 1, y: 2, z: 3};
+console.log(test1);
+alert("Упражнение №1. В консоль выведется тип данных object и это объект");
+
+/* Ex. 2
+let test2 = {x: 1, y: 2, z: 3};
+console.log(test2.x); */
+let test2 = {x: 1, y: 2, z: 3};
+console.log(test2);
+alert("Упражнение №2. В консоль выведется тип данных object и это объект");
+
+/* Ex. 3
+let test3 = [1, 2, 3];
+console.log(test3); */
+let test3 = [1, 2, 3];
+console.log(test3);
+alert("Упражнение №3. В консоль выведется тип данных object(массив) и это объект");
+
+/* Ex. 4
+let test4 = [1, 2, 3];
+console.log(test4[1]); */
+let test4 = [1, 2, 3];
+console.log(test4[1]);
+alert("Упражнение №4. В консоль выведется тип данных number и это примитив");
+
+/* Ex. 5
+let test51 = [1, 2, 3];
+let test52 = 1;
+console.log(test51); */
+let test51 = [1, 2, 3];
+let test52 = 1;
+console.log(test51);;
+alert("Упражнение №5. В консоль выведется тип данных object(массив) и это объект");
+
+/* Ex. 6
+let test61 = [1, 2, 3];
+let test62 = 1;
+console.log(test61[test62]); */
+let test61 = [1, 2, 3];
+let test62 = 1;
+console.log(test61[test62]);
+alert("Упражнение №6. В консоль выведется тип данных number и это примитив");
