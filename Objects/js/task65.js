7cf71f29f115771123d9109acd446a6d07acac2c
nickruden <1nikitos.ru2018@gmail.com>
2023-02-19 16:34:01 +0300

    Массивы как объекты в JavaScript(Task_65)

---
+++ Objects/js/task65.js
@@ -0,0 +1,30 @@
+/* Ex. 1
+console.log( typeof {x: 1, y: 2, z: 3} ); */
+console.log( typeof {x: 1, y: 2, z: 3} );  
+alert("Упражнение №1. В консоль выведется object");
+
+/* Ex. 2
+console.log( typeof [1, 2, 3] ); */
+console.log( typeof [1, 2, 3] );  
+alert("Упражнение №2. В консоль выведется object");
+
+/* Ex. 3
+let arr1 = [1, 2, 3];
+console.log( typeof arr1 ); */
+let arr1 = [1, 2, 3];
+console.log( typeof arr1 );  
+alert("Упражнение №3. В консоль выведется object");
+
+/* Ex. 4
+let arr2 = [1, 2, 3];
+console.log( typeof arr2[0] ); */
+let arr2 = [1, 2, 3];
+console.log( typeof arr2[0] ); 
+alert("Упражнение №4. В консоль выведется number");
+
+/* Ex. 5
+let arr3 = ['1', '2', '3'];
+console.log( typeof arr3[0] ); */
+let arr3 = ['1', '2', '3'];
+console.log( typeof arr3[0] );  
+alert("Упражнение №5. В консоль выведется string");
