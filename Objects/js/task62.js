45382cd9e4e36689dfc08da1c0560fc696379ed2
nickruden <1nikitos.ru2018@gmail.com>
2023-02-19 16:13:46 +0300

    Оператор in в JavaScript(Task_62)

---
+++ Objects/js/task62.js
@@ -0,0 +1,10 @@
+/* Ex. 1
+let obj = {x: 1, y: 2, z: 3};
+
+console.log('x' in obj);
+console.log('w' in obj); */ 
+let obj = {x: 1, y: 2, z: 3};
+
+console.log('x' in obj);
+console.log('w' in obj);
+alert("Упражнение №1. В консоль выведется: в первом случае - true, во втором - false, так как никакого свойства 'w' в объекте нет.")
