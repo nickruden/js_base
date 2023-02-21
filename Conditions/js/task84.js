7b6595826c3f9ec3c6832a0e2aa4120bf8bdce97
nickruden <1nikitos.ru2018@gmail.com>
2023-02-21 21:01:49 +0300

    Группировка условий в JavaScript(Task_84)

---
+++ Conditions/js/task84.js
@@ -0,0 +1,28 @@
+// Ex. 1
+let num1 = 3;
+		
+if ( (num1 > 5 && num1) < 10 || num1 == 20) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+
+// Ex. 2
+let num2 = 3;
+		
+if (num2 > 5 || (num2 > 0 && num2 < 3) ) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+
+// Ex. 3
+let num3 = 3;
+		
+if (num3 == 9 || (num3 > 10 && num3 < 20) || (num3 > 20 && num3 < 30) ) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
