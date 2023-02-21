29b2ed439e8fda8bde1a62560de3e079f99c27b8
nickruden <1nikitos.ru2018@gmail.com>
2023-02-21 12:34:21 +0300

    Равенство по значению и типу в JavaScript(Task_79)

---
+++ Conditions/js/task79.js
@@ -0,0 +1,63 @@
+// Ex. 1
+let test1 = '3';
+let test2 = '3';
+		
+if (test1 == test2) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №1. В консоль выведется '+++'");
+
+
+// Ex. 2
+let test21 = '3';
+let test22 = '3';
+		
+if (test21 === test22) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №2. В консоль выведется '+++'");
+
+
+// Ex. 3
+let test31 = 3;
+let test32 = '3';
+		
+if (test31 == test32) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №3. В консоль выведется '+++'");
+
+
+// Ex. 4
+let test41 = 3;
+let test42 = '3';
+		
+if (test41 === test42) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №4. В консоль выведется '---'");
+
+
+// Ex. 5
+let test51 = 3;
+let test52 = 3;
+		
+if (test51 === test52) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №5. В консоль выведется '+++'");
