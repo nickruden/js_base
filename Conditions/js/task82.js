5191a0b7af282ccc0dd18369eb01136fa52d6734
nickruden <1nikitos.ru2018@gmail.com>
2023-02-21 20:39:27 +0300

    Логическое ИЛИ в JavaScript(Task_82)

---
+++ Conditions/js/task82.js
@@ -0,0 +1,98 @@
+// Ex. 1
+let num11 = -10;
+let num12 = -10;
+
+if (num11 >= 0 || num12 >= 0) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №1. В консоль выведется '---'");
+
+
+// Ex. 2
+let num21 = 0;
+let num22 = 0;
+
+if (num21 >= 0 || num22 >= 0) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №2. В консоль выведется '+++'");
+
+
+// Ex. 3
+let num31 = 0;
+let num32 = 5;
+
+if (num31 >= 0 || num32 >= 0) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №3. В консоль выведется '+++'");
+
+
+// Ex. 4
+let num41 = 5;
+let num42 = 5;
+
+if (num41 >= 0 || num42 >= 0) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №4. В консоль выведется '+++'");
+
+
+// Ex. 5
+let num51 = -5;
+let num52 = 15;
+
+if (num51 >= 0 || num52 >= 0) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №5. В консоль выведется '+++'");
+
+
+// Ex. 6
+let num6 = 1;
+
+if (num6 == 0 || num6 == 1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №6. В консоль выведется '+++'");
+
+
+// Ex. 7
+let num7 = 2;
+
+if (num7 == 0 || num7 == 1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №7. В консоль выведется '---'");
+
+
+// Ex. 8
+let num8 = 2;
+
+if (num8 == 0 || num8 == 1 || num8 == 2) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+alert("Упражнение №8. В консоль выведется '+++'");
