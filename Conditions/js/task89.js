e57542c542cdf4d6d9cb7912538a7a8c7224a58d
nickruden <1nikitos.ru2018@gmail.com>
2023-02-23 16:48:40 +0300

    Сокращенная форма проверки на ложь в JavaScript(Task_89)

---
+++ Conditions/js/task89.js
@@ -0,0 +1,49 @@
+/* Ex. 1
+let test1 = true;
+
+if (test1 == false) {
+	console.log('+++');
+} else {
+	console.log('---');
+} */
+let test1 = true;
+
+if (!test1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+
+/* Ex. 2
+let test2 = true;
+
+if (test2 != true) {
+	console.log('+++');
+} else {
+	console.log('---');
+} */
+let test2 = true;
+
+if (!test2) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+
+/* Ex. 3
+let test3 = true;
+
+if (test3 != false) {
+	console.log('+++');
+} else {
+	console.log('---');
+} */
+let test3 = true;
+
+if (test3) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
