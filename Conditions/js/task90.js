fe95432c58851338c8c8738996a34613a3f1d21f
nickruden <1nikitos.ru2018@gmail.com>
2023-02-23 16:56:33 +0300

    Общая сокращенная форма проверки в JavaScript(Task_90)

---
+++ Conditions/js/task90.js
@@ -0,0 +1,118 @@
+// Ex. 1
+let test1 = 3;
+
+if (test1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №1. В консоль выведется '+++'");
+
+
+// Ex. 2
+let test2 = 'abc';
+
+if (test2) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №2. В консоль выведется '+++'");
+
+
+// Ex. 3
+let test3 = '';
+
+if (test3) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №3. В консоль выведется '---'");
+
+
+// Ex. 4
+let test4 = 3 * 'abc';
+
+if (test4) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №4. В консоль выведется '---'");
+
+
+// Ex. 5
+let test5 = null;
+
+if (test5) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №5. В консоль выведется '---'");
+
+
+// Ex. 6
+let test6 = false;
+
+if (test6) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №6. В консоль выведется '---'");
+
+
+// Ex. 7
+let test7;
+
+if (test7) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №7. В консоль выведется '---'");
+
+
+// Ex. 8
+let test8 = 0;
+
+if (test8) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №8. В консоль выведется '---'");
+
+
+// Ex. 9
+let test9 = '0';
+
+if (test9) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №9. В консоль выведется '+++'");
+
+
+// Ex. 10
+let test10 = -1;
+
+if (test10) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №10. В консоль выведется '+++'");
