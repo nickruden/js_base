f0da0f4d738189cae3eb353c6d4856d20e52632b
nickruden <1nikitos.ru2018@gmail.com>
2023-02-23 16:40:52 +0300

    Сокращенная форма проверки на истину в JavaScript(Task_88)

---
+++ Conditions/js/task88.js
@@ -0,0 +1,15 @@
+/* Ex. 1
+let test = true;
+
+if (test === true) {
+	console.log('+++');
+} else {
+	console.log('---');
+} */
+let test = true;
+
+if (test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
