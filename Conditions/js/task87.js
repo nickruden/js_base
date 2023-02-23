8ef7aaa90ed07167e62130d79f15e777ed58e7db
nickruden <1nikitos.ru2018@gmail.com>
2023-02-23 16:37:20 +0300

    Сравнение значений с булевым типом в JavaScript(Task_87)

---
+++ Conditions/js/task87.js
@@ -0,0 +1,82 @@
+// Ex. 1
+let test1 = 1;
+		
+if (test1 == true) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №1. В консоль выведется '+++'");
+
+
+// Ex. 2
+let test2 = 0;
+		
+if (test2 == true) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №2. В консоль выведется '---'");
+
+
+// Ex. 3
+let test3 = 1;
+		
+if (test3 == false) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №3. В консоль выведется '---'");
+
+
+// Ex. 4
+let test4 = 1;
+		
+if (test4 != true) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №4. В консоль выведется '---'");
+
+
+// Ex. 5
+let test5 = '';
+		
+if (test5 == false) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №5. В консоль выведется '+++'");
+
+
+// Ex. 6
+let test6;
+		
+if (test6 == true) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №6. В консоль выведется '---'");
+
+
+// Ex. 7
+let test7 = 3 * 'a';
+		
+if (test7 == true) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+alert("Упражнение №7. В консоль выведется '---'");
