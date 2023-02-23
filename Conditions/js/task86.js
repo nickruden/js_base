61be0c9fed1d015db7f37808bbf84fcc9e425789
nickruden <1nikitos.ru2018@gmail.com>
2023-02-23 16:22:06 +0300

    Условия с булевыми значениями в JavaScript(Task_86)

---
+++ Conditions/js/task86.js
@@ -0,0 +1,18 @@
+// Ex. 1
+let test1 = true;
+
+if (test1 === true) {
+    console.log("Упражнение №1. Значение переменной равняется 'true'");
+} else {
+    console.log("Упражнение №1. Значение переменной равняется 'false'");
+}
+
+
+// Ex. 2
+let test2 = false;
+
+if (test2 === false) {
+    console.log("Упражнение №2. Значение переменной равняется 'false'");
+} else {
+    console.log("Упражнение №2. Значение переменной равняется 'true'");
+}
