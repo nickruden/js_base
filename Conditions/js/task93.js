26ba29abddd13f8a3a5e948c3252a87eb0d9f9cf
nickruden <1nikitos.ru2018@gmail.com>
2023-02-26 16:25:13 +0300

    Необязательность фигурных скобок в JavaScript(Task_93)

---
+++ Conditions/js/task93.js
@@ -0,0 +1,17 @@
+/* Ex. 1
+if (test > 0) {
+	console.log('+++');
+} else {
+	console.log('---');
+} */
+let test = 10;
+if (test > 0) console.log('+++'); 
+else console.log('---');
+
+
+/* Ex. 2
+if (test > 0) {
+	console.log('+++');
+} */
+if (test > 0)
+	console.log('+++');
