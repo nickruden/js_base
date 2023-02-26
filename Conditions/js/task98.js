9a58f039a2a1128ff1d9ceee2a40149af24bc29c
nickruden <1nikitos.ru2018@gmail.com>
2023-02-26 17:09:06 +0300

    Тернарный оператор в JavaScript(Task_98)

---
+++ Conditions/js/task98.js
@@ -0,0 +1,18 @@
+/* Ex. 1
+let num = 1;
+let res;
+
+if (num >= 0) {
+	res = '1';
+} else {
+	res = '2';
+}
+
+console.log(res); */
+
+let num = 1;
+let res;
+
+res = num >= 0 ? '1' : '2';
+
+console.log(res);
