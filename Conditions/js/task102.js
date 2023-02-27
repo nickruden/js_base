7d826627d784b86801f55239315ab3efbc4859b3
nickruden <1nikitos.ru2018@gmail.com>
2023-02-27 21:21:20 +0300

    Нюансы области видимости переменных в конструкциях if-else в JavaScript(Task_102)

---
+++ Conditions/js/task102.js
@@ -0,0 +1,112 @@
+/* Ex. 1
+let age1 = 17;
+let adult1;
+
+if (age1 >= 18) {
+	let adult1 = true;
+} else {
+	let adult1 = false;
+}
+
+console.log(adult1); */
+
+let age1 = 17;
+let adult1;
+
+if (age1 >= 18) {
+	adult1 = true;
+} else {
+	adult1 = false;
+}
+
+console.log(adult1);
+
+
+/* Ex. 2
+let age2 = 17;
+let adult2;
+
+if (age2 >= 18) {
+	adult2 = true;
+} else {
+	let adult2 = false;
+}
+
+console.log(adult2); */
+
+let age2 = 17;
+let adult2;
+
+if (age2 >= 18) {
+	adult2 = true;
+} else {
+	adult2 = false;
+}
+
+console.log(adult2);
+
+
+/* Ex. 3
+let age3 = 17;
+let res3;
+
+if (age3 >= 18) {
+	if (age3 <= 23) {
+		let res3 = 'от 18 до 23';
+	} else {
+		let res3 = 'больше 23';
+	}
+} else {
+	let res3 = 'меньше 18';
+}
+
+console.log(res3); */
+
+let age3 = 17;
+let res3;
+
+if (age3 >= 18) {
+	if (age3 <= 23) {
+		res3 = 'от 18 до 23';
+	} else {
+		res3 = 'больше 23';
+	}
+} else {
+	res3 = 'меньше 18';
+}
+
+console.log(res3);
+
+
+/* Ex. 4
+let age4 = 19;
+let res4;
+
+if (age4 >= 18) {
+	let res4;
+	
+	if (age4 <= 23) {
+		res4 = 'от 18 до 23';
+	} else {
+		res4 = 'больше 23';
+	}
+} else {
+	res4 = 'меньше 18';
+}
+
+console.log(res4); */
+
+let age4 = 19;
+let res4;
+
+if (age4 >= 18) {
+	if (age4 <= 23) {
+		res4 = 'от 18 до 23';
+	} else {
+		res4 = 'больше 23';
+	}
+} else {
+	res4 = 'меньше 18';
+}
+
+console.log(res4);
