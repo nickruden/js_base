2fa09de8558e911d64423486dc39652b916ba140
nickruden <1nikitos.ru2018@gmail.com>
2023-02-27 21:54:03 +0300

    Проверка символов строки в JavaScript(Task_105)

--- Conditions/js/task105.js
+++ Conditions/js/task105.js
@@ -5,12 +5,14 @@ if (str1[0] = 'a') {
     console.log("Упражнение №1. Строка начинается на символ 'a'");
 }
 
+
 // Ex. 2
 let str2 = 'afjxz';
 
 if (str2[str2.length-1] = 'x') {
     console.log("Упражнение №2. Строка заканчивается на символ 'x'");
 }
+
 
 // Ex. 3
 let str3 = 'bcdef';
