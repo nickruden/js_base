2510a71365b8b114ba37eaec1d5818307ac92313
nickruden <1nikitos.ru2018@gmail.com>
2023-02-27 21:03:26 +0300

    Область видимости переменных в if-else в JavaScript(Task_101)

---
+++ Conditions/js/task101.js
@@ -0,0 +1,21 @@
+/* Ex. 1
+let age = 17;
+
+if (age >= 18) {
+	let adult = true;
+} else {
+	let adult = false;
+}
+
+console.log(adult); */
+
+let age = 17;
+let adult;
+
+if (age >= 18) {
+	adult = true;
+} else {
+	adult = false;
+}
+
+console.log(adult);
