6c0214acd5fff35fa01d3895cadc3a5cf9c5992f
nickruden <1nikitos.ru2018@gmail.com>
2023-03-01 20:37:42 +0300

    For loop in JavaScript(Task_111)

---
+++ Cycles/js/task111.js
@@ -0,0 +1,24 @@
+// Ex. 1
+for (let i = 1; i <= 100; i++) {
+    console.log('1) ' + i);
+}
+
+// Ex. 2
+for (let i = 11; i <= 33; i++) {
+    console.log('2) ' + i);
+}
+
+// Ex. 3
+for (let i = 0; i <= 100; i += 2) {
+    console.log('3) ' + i);
+}
+
+// Ex. 4
+for (let i = 1; i <= 99; i += 2) {
+    console.log('4) ' + i);
+}
+
+// Ex. 5
+for (let i = 100; i >= 0; i--) {
+    console.log('5) ' + i);
+}
