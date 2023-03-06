ec49b1fa78f7f3caa0ba33a562eb40bc30bc31c7
nickruden <1nikitos.ru2018@gmail.com>
2023-03-06 17:59:38 +0300

    Цикл while в JavaScript(Task_114)

---
+++ Cycles/js/task114.js
@@ -0,0 +1,29 @@
+// Ex. 1
+let num1 = 1;
+
+while (num1 <= 100) {
+    console.log("1) " + num1);
+    num1++;
+}
+
+
+// Ex. 2
+let num2 = 11;
+
+while(num2 <= 33) {
+    console.log("2) " + num2);
+    num2++;
+}
+
+
+// Ex. 3
+let num3 = 4;
+let count = 0;
+
+while (num3 <= 1000) {
+    num3 = num3 * 3;
+    count++;
+}
+console.log("3) Количество итераций: " + count);
+console.log("3) Число: " + num3);
+
