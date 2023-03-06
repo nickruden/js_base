d326c28ada70bba71c67bb80f5f971cf39763a38
nickruden <1nikitos.ru2018@gmail.com>
2023-03-06 17:45:36 +0300

    Перебор объекта циклом for-in в JavaScript(Task_113)

---
+++ Cycles/js/task113.js
@@ -0,0 +1,14 @@
+// Ex. 1
+let obj1 = {x: 1, y: 2, z: 3};
+
+for (let key1 in obj1) {
+    console.log("1) Ключ объекта: " + key1);
+}
+
+
+// Ex. 2
+let obj2 = {x: 1, y: 2, z: 3};
+
+for (let key2 in obj2) {
+    console.log("2) Элемент объекта: " + obj2[key2]);
+}
