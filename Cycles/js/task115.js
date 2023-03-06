9858a997b2c6be7d84f350db4912d1ea4254751a
nickruden <1nikitos.ru2018@gmail.com>
2023-03-06 18:10:53 +0300

    Цикл for для массивов в JavaScript(Task_115)

---
+++ Cycles/js/task115.js
@@ -0,0 +1,32 @@
+// Ex. 1
+let arr1 = ['a', 'b', 'c', 'd', 'e'];
+
+for(let i = 0; i < arr1.length; i++) {
+    console.log("1) Элемент массива: " + arr1[i]);
+}
+
+
+// Ex. 2
+let arr2 = ['a', 'b', 'c', 'd', 'e'];
+
+for(let i = 1; i < arr2.length - 1; i++) {
+    console.log("2) Элемент массива: " + arr2[i]);
+}
+
+
+// Ex. 3
+let arr3 = ['a', 'b', 'c', 'd', 'e'];
+
+for(let i = arr3.length - 1; i >= 0; i--) {
+    console.log("3) Элемент массива: " + arr3[i]);
+}
+
+
+// Ex. 4
+let arr4 = ['a', 'b', 'c', 'd', 'e'];
+
+for (let i = 0; i <= arr4.length - 1; i++) {
+	console.log(arr4[i]);
+}
+
+
