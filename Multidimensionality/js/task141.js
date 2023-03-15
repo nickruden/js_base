c638d7eeec3b53d8a57e10b3c92434134cd52763
nickruden <1nikitos.ru2018@gmail.com>
2023-03-15 20:18:15 +0300

    Проблемы при заполнении многомерных массивов в JavaScript(Task_141)

---
+++ Multidimensionality/js/task141.js
@@ -0,0 +1,146 @@
+/* Ex. 1 
+let arr = [];
+
+for (let i = 0; i < 3; i++) {
+	for (let j = 1; j <= 5; j++) {
+		arr[i].push(j);
+	}
+}
+
+console.log(arr); */
+let arr1 = [];
+
+for (let i = 0; i < 3; i++) {
+    arr1[i] = [];
+
+	for (let j = 1; j <= 5; j++) {
+		arr1[i].push(j);
+	}
+}
+
+console.log(arr1);
+
+
+/* Ex. 2
+let arr = [];
+
+for (let i = 0; i < 3; i++) {
+	arr[i] = '';
+	
+	for (let j = 1; j <= 5; j++) {
+		arr[i].push(j);
+	}
+}
+
+console.log(arr); */
+let arr2 = [];
+
+for (let i = 0; i < 3; i++) {
+	arr2[i] = [];
+	
+	for (let j = 1; j <= 5; j++) {
+		arr2[i].push(j);
+	}
+}
+
+console.log(arr2);
+
+
+/* Ex. 3
+let arr = [];
+
+for (let i = 0; i < 3; i++) {
+	arr[i];
+	
+	for (let j = 1; j <= 5; j++) {
+		arr[i].push(j);
+	}
+}
+
+console.log(arr); */
+let arr3 = [];
+
+for (let i = 0; i < 3; i++) {
+	arr3[i] = [];
+	
+	for (let j = 1; j <= 5; j++) {
+		arr3[i].push(j);
+	}
+}
+
+console.log(arr3);
+
+
+/* Ex. 4
+let arr = [];
+
+for (let i = 0; i < 3; i++) {
+	arr[j] = [];
+	
+	for (let j = 1; j <= 5; j++) {
+		arr[i].push(j);
+	}
+}
+
+console.log(arr); */
+let arr4 = [];
+
+for (let i = 0; i < 3; i++) {
+	arr4[i] = [];
+	
+	for (let j = 1; j <= 5; j++) {
+		arr4[i].push(j);
+	}
+}
+
+console.log(arr4);
+
+
+/* Ex. 5
+let arr = [];
+
+for (let i = 0; i < 3; i++) {
+	arr = [];
+	
+	for (let j = 1; j <= 5; j++) {
+		arr[i].push(j);
+	}
+}
+
+console.log(arr); */
+let arr5 = [];
+
+for (let i = 0; i < 3; i++) {
+	arr5[i] = [];
+	
+	for (let j = 1; j <= 5; j++) {
+		arr5[i].push(j);
+	}
+}
+
+console.log(arr5);
+
+
+/* Ex. 6
+let arr = [];
+
+for (let i = 0; i < 3; i++) {
+	arr[i] = [];
+	
+	for (let j = 1; j <= 5; j++) {
+		arr.push(j);
+	}
+}
+
+console.log(arr); */
+let arr6 = [];
+
+for (let i = 0; i < 3; i++) {
+	arr6[i] = [];
+	
+	for (let j = 1; j <= 5; j++) {
+		arr6[i].push(j);
+	}
+}
+
+console.log(arr6);
