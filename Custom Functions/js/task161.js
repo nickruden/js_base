1de090e895838789af5a2e776741504db54933e7
nickruden <1nikitos.ru2018@gmail.com>
2023-03-25 13:14:31 +0300

    Инструкция return в JavaScript(Task_161)

---
+++ Custom Functions/js/task161.js
@@ -0,0 +1,14 @@
+// Ex. 1
+function mathnum3 (num) {
+    return num ** 3;
+}
+let res = mathnum3(3);
+console.log(res);
+
+// Ex. 2
+function mathsqrt (num) {
+    return Math.sqrt(num);
+}
+let numsqrt1 = mathsqrt(3);
+let numsqrt2 = mathsqrt(4);
+console.log(numsqrt1 + numsqrt2);
