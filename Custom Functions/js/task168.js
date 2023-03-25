064b79b2b2483f95ca9161004a238f972d3f9851
nickruden <1nikitos.ru2018@gmail.com>
2023-03-25 15:30:16 +0300

    Логические операторы без if в функциях JavaScript(Task_168)

---
+++ Custom Functions/js/task168.js
@@ -0,0 +1,49 @@
+/* Ex. 1 
+function func(a, b) {
+	if (a == b) {
+		return true;
+	} else {
+		return false;
+	}
+} */
+function func1(a, b) {
+	return a == b;
+}
+
+/* Ex. 2 
+function func(a, b) {
+	if (a != b) {
+		return true;
+	} else {
+		return false;
+	}
+} */
+function func2(a, b) {
+	return a != b;
+}
+
+
+/* Ex. 3 
+function func(a, b) {
+	if (a + b >= 10) {
+		return true;
+	} else {
+		return false;
+	}
+} */
+function func3(a, b) {
+	return a + b >= 10;
+}
+
+
+/* Ex. 4 
+function func(num) {
+	if (num >= 0) {
+		return true;
+	} else {
+		return false;
+	}
+} */ 
+function func4(num) {
+	return num >= 0;
+}
