93a7fd1eda29c2546314caa407f3bb5619fbc634
nickruden <1nikitos.ru2018@gmail.com>
2023-03-25 12:45:20 +0300

    Основы работы с пользовательскими функциями в JavaScript(Task_156)

---
+++ Custom Functions/js/task156.js
@@ -0,0 +1,15 @@
+// Ex. 1
+function name() {
+    console.log("Моё имя Никита");
+}
+name();
+
+// Ex. 2
+function sumnum() {
+    let summ = 0;
+    for (let i = 1; i <= 100; i++) {
+        summ += i;
+    }
+    console.log(summ);
+}
+sumnum();
