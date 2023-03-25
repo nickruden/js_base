0c459880070f3f516caa8a10b6b7351477904fc1
nickruden <1nikitos.ru2018@gmail.com>
2023-03-25 12:51:58 +0300

    Параметры функций в JavaScript(Task_157)

---
+++ Custom Functions/js/task157.js
@@ -0,0 +1,15 @@
+// Ex. 1
+function math3(num) {
+    console.log(num ** 3);
+}
+math3(5);
+
+// Ex. 2
+function checknum(num) {
+    if (num >= 0 ) {
+        console.log('+++');
+    } else {
+        console.log('---');
+    }
+}
+checknum(12);
