895794565f1ad9c5727b42b996b23fc9ae7b30b8
nickruden <1nikitos.ru2018@gmail.com>
2023-03-27 10:03:48 +0300

    Function expression и Function declaration в JavaScript(Task_181)

---
+++ Advanced Theory About Functions/js/task181.js
@@ -0,0 +1,30 @@
+// Ex. 1 Это объявление функции(Function Declaration)
+function func1() {
+	console.log('!');
+}
+
+// Ex. 2  Это функциональное выражение(Function Expression)
+let func2 = function() {
+	console.log('!');
+}
+
+// Ex. 3
+func3()
+function func3() {
+    console.log("Привет")
+}
+
+// Ex. 4
+func4()
+let func4 = function() {
+    console.log("Привет")
+}
+
+// Ex. 5
+let func51 = function() {console.log('!')};
+let func52 = function() {
+	console.log('!')
+};
+function func3() {
+	console.log('!')
+}
