fc633ebb6a4791e13edf985c2027a4188b2d10b2
nickruden <1nikitos.ru2018@gmail.com>
2023-03-27 12:29:37 +0300

    Вложенные функции в JavaScript(Task_187)

---
+++ Advanced Theory About Functions/js/task187.js
@@ -0,0 +1,12 @@
+// Ex. 1
+function func(num1, num2) {
+    function square(num) {
+        return num ** 2;
+    }
+    function cube(num) {
+        return num ** 3;
+    }
+
+    return square(num1) + cube(num2);
+}
+console.log(func(19, 22));
