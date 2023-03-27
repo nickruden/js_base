4e6b04da79fc7bfec6856974c4ced41c4d526342
nickruden <1nikitos.ru2018@gmail.com>
2023-03-27 10:36:26 +0300

    Нюансы функциональных выражений в JavaScript(Task_182)

---
+++ Advanced Theory About Functions/js/task182.js
@@ -0,0 +1,126 @@
+// Ex. 1  Это объявление функции(Function Declaration)
+function func1() {
+	console.log('!');
+}
+
+// Ex. 2  Это функциональное выражение(Function Expression)
+let func2 = function() {
+	console.log('!');
+}
+
+// Ex. 3  Это функциональное выражение(Function Expression)
++function() {
+	console.log('!');
+}
+
+// Ex. 4  Это функциональное выражение(Function Expression)
+!function func4() {
+	console.log('!');
+}
+
+// Ex. 5  Это функциональное выражение(Function Expression)
+-function func5() {
+	console.log('!');
+}
+
+// Ex. 6  Это функциональное выражение(Function Expression)
+1 + function func6() {
+	console.log('!');
+}
+
+// Ex. 7  Это функциональное выражение(Function Expression)
+(function func7() {
+	console.log('!');
+})
+
+// Ex. 8  Это функциональное выражение(Function Expression)
+console.log(
+	function() {
+		console.log('!');
+	}
+);
+
+// Ex. 9  Это функциональное выражение(Function Expression)
+let test9 = function func9() {
+	console.log('!');
+}
+
+// Ex. 10  Это функциональное выражение(Function Expression)
+console.log( 
+	function func10() {
+		console.log('!');
+	}
+);
+
+// Ex. 11  Это функциональное выражение(Function Expression)
++function func11() {
+	console.log('!');
+}
+
+// Ex. 12  Это объявление функции(Function Declaration)
+function func12() {
+	console.log('!');
+}
+
+// Ex. 13  Это функциональное выражение(Function Expression)
+-
+function func13() {
+	console.log('!');
+}
+
+// Ex. 14  Это объявление функции(Function Declaration)
+-1;
+function func14() {
+	console.log('!');
+}
+
+// Ex. 15  Это объявление функции(Function Declaration)
+-1
+function func15() {
+	console.log('!');
+}
+
+// Ex. 16  Это объявление функции(Function Declaration)
+1
+function func16() {
+	console.log('!');
+}
+
+// Ex. 17  Это функциональное выражение(Function Expression)
+-1-
+function func17() {
+	console.log('!');
+}
+
+// Ex. 18  Это объявление функции(Function Declaration)
+function func18() {
+	console.log('!');
+}
++1;
+
+// Ex. 19  Это объявление функции(Function Declaration)
+function func19() {
+	console.log('!');
+} + 1;
+
+// Ex. 20  Это функциональное выражение(Function Expression)
++function func20() {
+	console.log('!');
+} + 1;
+
+// Ex. 21  Это функциональное выражение(Function Expression)
++
+function func21() {
+	console.log('!');
+} + 1;
+
+// Ex. 22  Это объявление функции(Function Declaration)
++ 1
+function func22() {
+	console.log('!');
+} + 1;
+
+// Ex. 23  Это объявление функции(Function Declaration)
+function func23() {
+	console.log('!');
+} + console.log('!');
