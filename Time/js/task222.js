3d385e2a5d847d862157271e3e940b991175c4af
nickruden <1nikitos.ru2018@gmail.com>
2023-04-21 16:40:37 +0300

    Работа с объектом Date в JavaScript(Task_222)

---
+++ Time/js/task222.js
@@ -0,0 +1,11 @@
+// Ex. 1 
+let day = new Date();
+console.log("Текущий день: ", day.getDate()); 
+
+// Ex. 2 
+let month = new Date();
+console.log("Текущий месяц: ", day.getMonth()); 
+
+// Ex. 3 
+let year = new Date();
+console.log("Текущий год: ", day.getFullYear()); 
