4100187ab1dbf7faf6ae70bf47c84a0e38a6d77b
nickruden <1nikitos.ru2018@gmail.com>
2023-05-17 14:42:08 +0300

    Сравнение даты без года в JavaScript(Task_246)

---
+++ Time/js/task246.js
@@ -0,0 +1,11 @@
+// Ex. 1 
+let date1 = '09-21';
+let date2 = '09-23';
+
+if (date1 > date2) {
+    console.log('Дата ' + date1 + ' больше, чем ' + date2);
+} else if (date1 < date2) {
+    console.log('Дата ' + date2 + ' больше, чем ' + date1);
+} else {
+    console.log('Даты равны');
+};
