2b2ad3fbcff83c25e0520ff2e69862212cee8c88
nickruden <1nikitos.ru2018@gmail.com>
2023-05-17 14:38:33 +0300

    Строковое сравнение дат на JavaScript(Task_245)

---
+++ Time/js/task245.js
@@ -0,0 +1,11 @@
+// Ex. 1 
+let date1 = '2020-11-31';
+let date2 = '2020-12-01';
+
+if (date1 > date2) {
+    console.log('Дата ' + date1 + ' больше, чем дата ' + date2);
+} else if (date2 > date1) {
+    console.log('Дата ' + date2 + ' больше, чем дата ' + date1);
+} else {
+    console.log('Даты равны');
+};
