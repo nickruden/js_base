79fc7138c613bfd63f00fb0cbb2801d58ec62ebc
nickruden <1nikitos.ru2018@gmail.com>
2023-05-17 17:22:04 +0300

    Сложные селекторы DOM элемента в JavaScript(Task_252)

---
+++ Introduction to DOM/js/task252.js
@@ -0,0 +1,16 @@
+// Ex. 1 
+let p_1 = document.querySelector('#block p');
+
+console.log(p_1);
+
+
+// Ex. 2 
+let p_2 = document.querySelector('.block p');
+
+console.log(p_2);
+
+
+// Ex. 3 
+let p_3 = document.querySelector('.www');
+
+console.log(p_3);
