476eaca34c2fec13c5ef771d6007504577163de6
nickruden <1nikitos.ru2018@gmail.com>
2023-05-17 17:12:06 +0300

    Получение DOM элемента в JavaScript(Task_251)

---
+++ Introduction to DOM/js/task251.js
@@ -0,0 +1,8 @@
+// Ex. 1 
+let p1 = document.querySelector('#elem1');
+let p2 = document.querySelector('#elem2');
+let p3 = document.querySelector('#elem3');
+
+console.log(p1);
+console.log(p2);
+console.log(p3);
