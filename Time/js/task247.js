f41567872cb8280b260e09401b570e2bf785592f
nickruden <1nikitos.ru2018@gmail.com>
2023-05-17 14:53:24 +0300

    Попадание даты в промежуток(Task_247)

---
+++ Time/js/task247.js
@@ -0,0 +1,28 @@
+// Ex. 1 
+let date = '05-11';
+
+if (date >= '03-21' && date <= '04-20') {
+    console.log('Знак зодиака Овен');
+} else if (date >= '04-21' && date <= '05-21') {
+    console.log('Знак зодиака Телец');
+} else if (date >= '05-22' && date <= '06-21') {
+    console.log('Знак зодиака Близнецы');
+} else if (date >= '06-22' && date <= '07-22') {
+    console.log('Знак зодиака Рак');
+} else if (date >= '07-23' && date <= '08-23') {
+    console.log('Знак зодиака Лев');
+} else if (date >= '08-24' && date <= '09-23') {
+    console.log('Знак зодиака Дева');
+} else if (date >= '09-24' && date <= '10-23') {
+    console.log('Знак зодиака Весы');
+} else if (date >= '10-24' && date <= '11-22') {
+    console.log('Знак зодиака Скорпион');
+} else if (date >= '11-23' && date <= '12-22') {
+    console.log('Знак зодиака Стрелец');
+} else if (date >= '12-23' && date <= '01-20') {
+    console.log('Знак зодиака Козерог');
+} else if (date >= '01-21' && date <= '02-19') {
+    console.log('Знак зодиака Водолей');
+} else if (date >= '02-20' && date <= '03-20') {
+    console.log('Знак зодиака Рыбы');
+};
