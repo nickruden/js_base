1eb0884993a1c5db443907c6d50ceb9fdde2cf62
nickruden <1nikitos.ru2018@gmail.com>
2023-05-19 16:59:42 +0300

    Один обработчик ко многим элементам в JavaScript!(Task_255)

---
+++ Introduction to DOM/js/task255.js
@@ -0,0 +1,10 @@
+// Ex. 1 
+function func() {
+	console.log('message');
+}
+
+let p1 = document.querySelector('#wrapper__elem1').addEventListener('click', func);
+let p2 = document.querySelector('#wrapper__elem2').addEventListener('click', func);
+let p3 = document.querySelector('#wrapper__elem3').addEventListener('click', func);
+let p4 = document.querySelector('#wrapper__elem4').addEventListener('click', func);
+let p5 = document.querySelector('#wrapper__elem5').addEventListener('click', func); 
