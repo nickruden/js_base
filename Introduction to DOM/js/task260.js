443a9e9aa2734c5d2b2b6e011a741b53e25ea18c
nickruden <1nikitos.ru2018@gmail.com>
2023-05-19 18:40:19 +0300

    Атрибуты тегов как свойства в JavaScript!(Task_260)

---
+++ Introduction to DOM/js/task260.js
@@ -0,0 +1,22 @@
+// Ex. 1 
+let elem1 = document.querySelector('#elem1');
+
+let btn1 = document.querySelector('.wrapper__button1').addEventListener('click', () => console.log(elem1.type));
+
+
+// Ex. 2 
+let elem2 = document.querySelector('#elem2');
+
+let btn2 = document.querySelector('.wrapper__button2').addEventListener('click', () => elem2.type = 'submit');
+
+
+// Ex. 3 
+let elem3_1 = document.querySelector('#elem3_1');
+let elem3_2 = document.querySelector('#elem3_2');
+
+let btn3 = document.querySelector('.wrapper__button3').addEventListener('click', () => elem3_2.textContent = elem3_1.href);
+
+// Ex. 4 
+let elem4 = document.querySelector('#elem4');
+
+let btn4 = document.querySelector('.wrapper__button4').addEventListener('click', () => elem4.textContent += '(' + elem4.href + ')');
