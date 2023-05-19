fa2b54e53a9675e7872c86adf37b4eee8a4aee7e
nickruden <1nikitos.ru2018@gmail.com>
2023-05-19 16:48:41 +0300

    Привязывание обработчиков к элементам в JavaScript(Task_253)

---
+++ Introduction to DOM/js/task253.js
@@ -0,0 +1,8 @@
+// Ex. 1 
+let btn1 = document.querySelector('#wrapper__button1');
+let btn2 = document.querySelector('#wrapper__button2');
+let btn3 = document.querySelector('#wrapper__button3');
+
+btn1.addEventListener('click', () => alert('1'));
+btn2.addEventListener('click', () => alert('2'));
+btn3.addEventListener('click', () => alert('3'));
