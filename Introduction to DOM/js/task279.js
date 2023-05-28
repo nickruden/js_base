f3f0984b87eebe0c29e287a414cfcf496f592793
nickruden <1nikitos.ru2018@gmail.com>
2023-05-28 11:37:18 +0300

    Поиск элементов внутри другого элемента в JavaScript(Task_279)

---
+++ Introduction to DOM/js/task279.js
@@ -0,0 +1,5 @@
+// Ex. 1 
+let parent = document.querySelector('#parent');
+let elems1 = parent.querySelectorAll('.www');
+let elems2 = parent.querySelectorAll('.ggg');
+console.log(elems1, elems2); 
