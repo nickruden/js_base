d6ae6ed0245358b197b0acac39822cdf3947e61e
nickruden <1nikitos.ru2018@gmail.com>
2023-05-29 17:57:52 +0300

    Всплытие событий в JavaScript(Task_306)

---
+++ Object Event/js/task306.js
@@ -0,0 +1,14 @@
+// Ex. 1 
+let elem1 = document.querySelector('#elem1');
+let elem2 = document.querySelector('#elem2');
+let elem3 = document.querySelector('#elem3');
+
+elem1.addEventListener('click', function() {
+	console.log('зеленый');
+});
+elem2.addEventListener('click', function() {
+	console.log('голубой');
+});
+elem3.addEventListener('click', function() {
+	console.log('красный');
+}); 
