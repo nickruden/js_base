703811150455c2fbd7c58819d374b95040d4108e
nickruden <1nikitos.ru2018@gmail.com>
2023-05-30 14:46:02 +0300

    Погружение событий в JavaScript(Task_309)

---
+++ Object Event/js/task309.js
@@ -0,0 +1,25 @@
+// Ex. 1 
+let elem1 = document.querySelector('#elem1');
+let elem2 = document.querySelector('#elem2');
+let elem3 = document.querySelector('#elem3');
+
+elem1.addEventListener('click', function() {
+	console.log('зеленый - погружение');
+}, true);
+elem1.addEventListener('click', function() {
+	console.log('зеленый - всплытие');
+}, false);
+
+elem2.addEventListener('click', function() {
+	console.log('голубой - погружение');
+}, true);
+elem2.addEventListener('click', function() {
+	console.log('голубой - всплытие');
+}, false);
+
+elem3.addEventListener('click', function() {
+	console.log('красный - погружение');
+}, true);
+elem3.addEventListener('click', function() {
+	console.log('красный- всплытие');
+}, false); 
