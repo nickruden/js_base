efe1927cfdb96d01990b19f2e613db921e111a8a
nickruden <1nikitos.ru2018@gmail.com>
2023-05-30 15:03:32 +0300

    Потеря контекста в JavaScript(Task_311)

---
+++ Context/js/task311.js
@@ -0,0 +1,14 @@
+// Ex. 1 
+// "use strict";
+
+let elem = document.querySelector('#elem');
+elem.addEventListener('blur', parent);
+
+function parent() {
+	console.log(this.value); // выведет значение атрибута value элемента input
+	
+	function child() {
+		console.log(this.value); // выведет ошибку, так как this внутренней функции указывает на undefined при строгом режиме, а атрибутов у типа undefined нет
+	}
+	child();
+}; 
