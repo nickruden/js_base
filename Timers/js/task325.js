883ded33b12598c4ea8cbfbbc852f5fa0029a827
nickruden <1nikitos.ru2018@gmail.com>
2023-06-01 15:09:22 +0300

    Задержка перед выполнением в JavaScript(Task_325)

---
+++ Timers/js/task325.js
@@ -0,0 +1,25 @@
+// Функция setTimeout, которая позволяет сделать задержку перед запуском кода. Эта задержка, в отличии от функции setInterval, случится только один раз. Первым параметром setTimeout принимает исходный код функции, а вторым - задержку в миллисекундах перед запуском этой функции.
+
+// Ex. 1
+const p1 = document.querySelector("#p1");
+const text = p1.textContent;
+const num = Number(text);
+
+setTimeout(function () {
+square = num ** 2;
+p1.textContent = square;
+}, 10000);
+
+
+// Ex. 2
+let number = 0;
+
+function timer() {
+setTimeout(function () {
+    console.log(number++);
+
+    timer(); // вызовем сами себя
+}, 1000);
+}
+
+timer(); 
