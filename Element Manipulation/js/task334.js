aea3594bd324719c3da093dbc860e7430359644c
nickruden <1nikitos.ru2018@gmail.com>
2023-06-02 15:03:07 +0300

    Проверка элементов в JavaScript(Task_334)

---
+++ Element Manipulation/js/task334.js
@@ -0,0 +1,17 @@
+// Ex. 1 
+let elem1 = document.querySelector('#elem1');
+
+console.log(elem1.matches('.www'));
+
+
+// Ex. 2 
+let elem2 = document.querySelector('#elem2');
+
+console.log(elem1.matches('p'));
+
+
+// Ex. 3 
+let elem3_1 = document.querySelector('#elem3_1');
+let elem3_2 = document.querySelector('#elem3_2');
+
+console.log(elem3_1.contains(elem3_2)); 
