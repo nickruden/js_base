71310eeb26b18b6829736ee92529c0544896283a
nickruden <1nikitos.ru2018@gmail.com>
2023-06-03 17:47:46 +0300

    Прячем текст при редактировании элемента на JavaScript(Task_346)

---
+++ Practice/js/task346.js
@@ -0,0 +1,17 @@
+// Ex. 1 
+let elem = document.querySelector("#elem");
+
+elem.addEventListener("click", function func() {
+	let input = document.createElement("input");
+	input.value = elem.textContent;
+
+	elem.textContent = "";
+	elem.appendChild(input);
+
+	input.addEventListener("blur", function () {
+    	elem.textContent = this.value;
+    	elem.addEventListener("click", func);
+	});
+
+	elem.removeEventListener("click", func);
+}); 
