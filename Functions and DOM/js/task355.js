5567691e82ab022b7155a78e371972a2654dec0a
nickruden <1nikitos.ru2018@gmail.com>
2023-06-04 20:52:22 +0300

    Функции для работы с группой DOM элементов на JavaScript(Task_355)

---
+++ Functions and DOM/js/task355.js
@@ -0,0 +1,10 @@
+// Ex. 1 
+function appendText(selector, text) {
+    let elems = document.querySelectorAll(selector);
+
+    for (let elem of elems) {
+        elem.innerHTML += text;
+    };
+};
+
+appendText('#elem', '!'); 
